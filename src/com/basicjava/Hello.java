2b4dd8f2b2776656a39d16d0c10730e82d83a73d
Harshu5534 <girishpati2611@gmail.com>
2023-06-12 13:08:04 +0530

    java file added successfully

---
+++ src/com/basicjava/Hello.java
@@ -0,0 +1,10 @@
+package com.basicjava;
+
+import javax.xml.transform.stream.StreamSource;
+
+public class Hello{
+    public static void main(String[] args) {
+        System.out.println("Welcome to the java world");
+    }
+}
+
