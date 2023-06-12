ba9ad091e5b6629cf0682761d4cf2f6399fa51e0
Harshu5534 <girishpati2611@gmail.com>
2023-06-12 13:38:13 +0530

    Another file added

---
+++ src/com/basicjava/AllDataTypes.java
@@ -0,0 +1,19 @@
+import java.sql.SQLOutput;
+
+public class AllDataTypes {
+    public static void main(String[] args) {
+        String Name= "Girish" ;
+        int age=24;
+        float heightInFeet=6.6f;
+        double weight=59.5;
+        boolean y;
+        char gender='M';
+        System.out.println("My Name is "+ Name);
+        System.out.println("My age is "+ age);
+        System.out.println("My Height is feet is "+ heightInFeet);
+        System.out.println("My weight is "+ weight);
+        System.out.println("My gender is male " +gender);
+
+
+    }
+}
